0db279c7de475c25c3ecff5f0d61dbda75b50ec3
Erik M. Bray <erik.bray@lri.fr>
2016-06-15 14:56:25 +0200

    Fix hard-coded trac.sagemath.org in buildbot_hook (also minor bug where _get_build returns () not None)
    That said we're not using this component anymore so it's not well tested.

--- sage_trac/buildbot_hook.py
+++ sage_trac/buildbot_hook.py
@@ -143,7 +143,9 @@ class BuildbotHook(git_merger.GitMerger):
                 }
 
         if tracid is not None:
-            change['comments'] = u'From Trac #{tracid} (http://trac.sagemath.org/{tracid})'.format(tracid=unicode(tracid))
+            change['comments'] = \
+                    u'From Trac #{tracid} ({base}/{tracid})'.format(
+                            base=self.env.base_url, tracid=unicode(tracid))
             change['properties']['trac_ticket'] = tracid
 
         for key in change:
@@ -233,7 +235,7 @@ class BuildbotHook(git_merger.GitMerger):
 
         build = self._get_build(req, ticket, True)
 
-        if build is None:
+        if not build:
             return stream
 
         builder, number, rc = build
