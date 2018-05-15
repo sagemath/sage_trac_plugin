06af21ba2a308653835fe2e333bdb531dd71efb7
Erik M. Bray <erik.bray@lri.fr>
2018-05-15 18:17:46 +0200

    completing v1.0.3 release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='sage_trac',
-        version='1.0.3',
+        version='1.0.4.dev',
         url='https://github.com/sagemath/sage_trac_plugin',
         packages=find_packages(),
         zip_safe=True,
